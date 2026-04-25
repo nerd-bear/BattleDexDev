967423c45418636fbaee5d5c29a92cf4e39a1348
nerd-bear <nerd.bear.dev@gmail.com>
2026-04-25 12:57:40 +0100

    bot token fixed I think :(

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = ""
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_PATH = "cards.db"
 CARD_FILE = "data/card.json"
-MAIN_GUILD_ID = 1495045506429227178  
+
 if not BOT_TOKEN:
-    raise RuntimeError("DISCORD_BOT_TOKEN is not set.")
+    raise RuntimeError("BOT_TOKEN is not set.")
