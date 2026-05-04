3e03850372bce79285ada24a79db587f8cdefbd6
nerd-bear <nerd.bear.dev@gmail.com>
2026-05-04 16:05:29 +0100

    QoL image fix

--- cogs/qol.py
+++ cogs/qol.py
@@ -47,7 +47,7 @@ class QoLCog(commands.Cog):
 
         # 3. Handle the image attachment
         # Make sure the file is actually on your machine at this path
-        file_path = "0f663482e9a64b46e77775806ca60479.png" 
+        file_path = "assets/promotional/promo_banner_alpha.png" 
         try:
             image_file = disnake.File(file_path, filename="battledex.png")
             embed.set_image(url="attachment://battledex.png")
